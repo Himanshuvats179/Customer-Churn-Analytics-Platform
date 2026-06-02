e630955f81ac97cf4cedef69809d3d3f9ea117b7
himanshuvats179 <hh21csb0b222@student.nitw.ac.in>
2026-06-03 05:11:06 +0530

    fix(backend): restore get_db session dependency and add psycopg2-binary for PostgreSQL support

--- app/database.py
+++ app/database.py
@@ -1,12 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///churn.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+try:
+    # DATABASE_URL = "postgresql+psycopg2://user:password@host:5432/churndb"
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
+
+    with engine.connect() as conn:
+        print("Connected to PostgreSQL")
+
+except Exception:
+    print("PostgreSQL unavailable, using SQLite")
+
+    DATABASE_URL = "sqlite:///churn.db"
+
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -19,9 +36,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
-        db.close()
+        db.close()
